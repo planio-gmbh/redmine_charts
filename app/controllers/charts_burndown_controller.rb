6d07ca13e25d6e9784d4a3e71566ecc587d50400
Maciej Szczytowski <mszczytowski@gmail.com>
2009-04-21 10:16:22 +0200

    merging sql optimalization

--- app/controllers/charts_burndown_controller.rb
+++ app/controllers/charts_burndown_controller.rb
@@ -14,42 +14,67 @@ class ChartsBurndownController < ChartsController
     done = []
     max = 0
 
-    conditions_sql = "project_id in (?) and (start_date <= ? or (start_date is null and created_on <= ?))"
+    # get data for estimated hours
+
+    j = 0
+    estimated_hours_rows = Issue.all(:select => "sum(issues.estimated_hours) as hours, case when start_date is null then created_on else start_date end as issue_date", :conditions => conditions, :group => :issue_date, :order => :issue_date)
 
     prepare_ranged[:dates].each_with_index do |date,i|
-      hours = Issue.sum(:estimated_hours, :conditions => [conditions_sql, conditions[:project_id], date[1], date[1]])
+      hours = i > 0 ? estimated[i-1][0] : 0
+      for k in j..(estimated_hours_rows.size - 1)
+        if estimated_hours_rows[k].issue_date.to_time <= date[1]
+          hours += estimated_hours_rows[k].hours.to_i
+          j += 1
+        else
+          break
+        end
+      end
+                        
       estimated[i] = [hours, l(:charts_burndown_hint_estimated, { :estimated_hours => hours })]
       max = hours if max < hours
     end
 
+    # get data for logged hours
+    
+    j = 0
+    logged_hours_rows = TimeEntry.all(:select => "sum(time_entries.hours) as hours, spent_on", :conditions => conditions, :group => :spent_on, :order => :spent_on)
+
     prepare_ranged[:dates].each_with_index do |date,i|
-      hours = TimeEntry.sum(:hours, :conditions => ["project_id in (?) and spent_on <= ?", conditions[:project_id], date[1]])
+      hours = i > 0 ? logged[i-1][0] : 0
+      for k in j..(logged_hours_rows.size - 1)
+        if logged_hours_rows[k].spent_on.to_time <= date[1]
+          hours += logged_hours_rows[k].hours.to_i
+          j += 1
+        else
+          break
+        end
+      end    
       logged[i] = [hours, l(:charts_burndown_hint_logged, { :logged_hours => hours })]
       max = hours if max < hours
     end
 
-    prepare_ranged[:dates].each_with_index do |date,i|
-      hours = estimated[i][0]
-      issues = Issue.find(:all, :conditions => [conditions_sql, conditions[:project_id], date[1], date[1]])
-      total_ratio = 0
-      issues.each do |issue|
-        journal = issue.journals.find(:first, :conditions => ["created_on <= ? and journal_details.prop_key = 'done_ratio'", date[1]], :order => "created_on desc", :select => "journal_details.value", :joins => "left join journal_details on journal_details.journal_id = journals.id")
-        ratio = journal ? journal.value.to_i : 0
-        total_ratio += ratio
-        hours -= issue.estimated_hours.to_f * ratio.to_f / 100 if issue.estimated_hours
-      end
-      done[i] = issues.size > 0 ? Integer(total_ratio/issues.size) : 0
-      remaining[i] = [hours, l(:charts_burndown_hint_remaining, { :remaining_hours => hours, :work_done => done[i] })]
-    end
+    # get data for done ratio, remaining and predicted hours
 
-    prepare_ranged[:dates].each_with_index do |date,i|
-      hours = logged[i][0] + remaining[i][0]
-      if hours > estimated[i][0]
-        predicted[i] = [hours, l(:charts_burndown_hint_predicted_over_estimation, { :remaining_hours => hours, :hours_over_estimation => hours - estimated[i][0] }), true] # prepare_ranged[:labels][i]]), true]
-      else
-        predicted[i] = [hours, l(:charts_burndown_hint_predicted, { :remaining_hours => hours })]
+    prepare_ranged[:dates].each_with_index do |date,i|    
+      rows = JournalDetail.find_by_sql(["select sum(journal_details.value*issues.estimated_hours)/sum(issues.estimated_hours) as ratio_done from (select max(journal_details.id) as journal_details_id, journals.journalized_id as issue_id from issues left join journals on journals.journalized_id = issues.id and journals.journalized_type ='Issue' left join journal_details on journals.id = journal_details.journal_id and journal_details.prop_key = 'done_ratio' where issues.project_id in (?) and (journals.id is null or journals.created_on <= ?) group by issues.id) left join issues on issues.id = issue_id left join journal_details on journal_details.id = journal_details_id", conditions[:project_id], date[1]])
+      if rows 
+        ratio_done = rows[0].ratio_done
       end
-      max = hours if max < hours
+      done[i] = ratio_done.to_i
+      
+      remaining_hours = done[i] > 0 ? logged[i][0]/done[i]*(100-done[i]) : estimated[i][0]
+      
+      remaining[i] = [remaining_hours, l(:charts_burndown_hint_remaining, { :remaining_hours => remaining_hours, :work_done => done[i] })]
+      
+      predicted_hours = logged[i][0] + remaining[i][0]
+      
+      if predicted_hours.to_i > estimated[i][0].to_i
+        predicted[i] = [predicted_hours, l(:charts_burndown_hint_predicted_over_estimation, { :remaining_hours => predicted_hours, :hours_over_estimation => predicted_hours - estimated[i][0] }), true]
+      else
+        predicted[i] = [predicted_hours, l(:charts_burndown_hint_predicted, { :remaining_hours => predicted_hours })]
+      end
+      
+      max = predicted_hours if max < predicted_hours
     end
 
     sets = [
