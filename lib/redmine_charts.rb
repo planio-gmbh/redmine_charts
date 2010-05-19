b78c94f398074c4ee4f7ab2d5935ed12af6949d2
Maciej Szczytowski <mszczytowski@gmail.com>
2010-05-19 19:38:39 +0200

    Enable subtasks for Redmine 0.9.4.devel

--- lib/redmine_charts.rb
+++ lib/redmine_charts.rb
@@ -11,7 +11,7 @@ require 'redmine_charts/time_entry_patch'
 module RedmineCharts
 
   def self.has_sub_issues_functionality_active
-    (Redmine::VERSION.to_a <=> [0,9,5]) >= 0            
+    (Redmine::VERSION.to_a <=> [0,9,4,'devel']) >= 0
   end
 
 end
