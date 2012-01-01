dcab14ba05a0ccc0302cc8aac9d0d0a30477ed15
idlysphere <idlysphere@mac.com>
2012-01-01 08:59:21 +0800

    Fix remaining hours calculation without logged hours

--- app/controllers/charts_burndown_controller.rb
+++ app/controllers/charts_burndown_controller.rb
@@ -109,7 +109,7 @@ class ChartsBurndownController < ChartsController
         estimated = estimated_hours_per_issue[issue.id] ? estimated_hours_per_issue[issue.id][index] : 0
         done_ratio = done_ratios_per_issue[issue.id] ? done_ratios_per_issue[issue.id][index] : 0
 
-        total_remaining_hours[index] += done_ratio > 0 ? logged/done_ratio*(100-done_ratio) : estimated
+        total_remaining_hours[index] += ((done_ratio > 0 and logged > 0) ? (logged/done_ratio) : (estimated/100)) * (100-done_ratio)
 
         total_logged_hours[index] += logged
         total_estimated_hours[index] += estimated
