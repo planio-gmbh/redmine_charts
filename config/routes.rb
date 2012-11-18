dd4c56a34ad651e754c93832d9d1f5786b74e11f
drakontia <mhden@drakontia.com>
2012-11-18 09:05:06 +0900

    compatible routes with Redmine 2.0.x

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,16 @@ require 'redmine_charts/utils'
 
 # Configuring routing for plugin's controllers.
 
-ActionController::Routing::Routes.draw do |map|
-  RedmineCharts::Utils.controllers_for_routing do |name, controller|
-    map.connect "projects/:project_id/charts/#{name}/:action", :controller => controller 
+if Rails::VERSION::MAJOR < 3
+  ActionController::Routing::Routes.draw do |map|
+    RedmineCharts::Utils.controllers_for_routing do |name, controller|
+      map.connect "projects/:project_id/charts/#{name}/:action", :controller => controller
+    end
+  end
+else
+  RedmineApp::Application.routes.draw do
+    RedmineCharts::Utils.controllers_for_routing do |name, controller|
+      match "projects/:project_id/charts/#{name}/:action", :to => controller, :via => [:get]
+    end
   end
 end
