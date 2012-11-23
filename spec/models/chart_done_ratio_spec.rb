5ae0e3eb0813d12924d4eb75c011241e9e786137
drakontia <mhden@drakontia.com>
2012-11-24 04:10:27 +0900

    Fixtures can't generate done_ratio by IssuePatch.
    So, we should prepare chart_done_ratio row.

--- spec/models/chart_done_ratio_spec.rb
+++ spec/models/chart_done_ratio_spec.rb
@@ -3,17 +3,16 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe ChartDoneRatio do
 
   before(:all) do
-    @aggregation = ChartDoneRatio.get_aggregation_for_issue(:project_ids => [15041])
+    @aggregation = ChartDoneRatio.get_aggregation_for_issue(:project_ids => 15041)
     Time.set_current_date = Time.mktime(2010,4,1)
     @timeline = ChartDoneRatio.get_timeline_for_issue({:project_ids => 15041}, {:range => :weeks, :limit => 11, :offset => 0})
   end
 
   it "should aggregation" do
-    aggregation = ChartDoneRatio.get_aggregation_for_issue(:project_ids => [15041])
-    aggregation[15041].should == 100
-    aggregation[15043].should == 60
-    aggregation[15044].should == 100
-    aggregation[15045].should == 60
+    @aggregation[15041].should == 100
+    @aggregation[15043].should == 60
+    @aggregation[15044].should == 60
+    @aggregation[15045].should == 60
   end
 
   it "should timeline" do
